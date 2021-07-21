ffc3c0d39fd6cefb004a10301b2e6b1f9a0408c6
Eduardo <eduti.snpr@gmail.com>
2021-07-21 15:41:41 -0300

    Adicionar arquivos de projeto.

---
+++ store/Controllers/ProdutoController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using store.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace store.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public ActionResult Visualizar()
+        {
+            Produto produto =  GetProduto();
+            return View(produto);
+
+            /*
+            return new ContentResult()
+            {
+                Content = "<h3>Produto -> Visualizar</h3>",
+                ContentType = "text/html"
+            };
+            */
+        }
+
+        private Produto GetProduto()
+        {
+            return new Produto()
+            {
+                Id = 1,
+                Nome = "Xbox One X",
+                Descricao = "Console Xbox One X",
+                //M é decimal
+                Valor = 2000.00M
+            };
+        }
+    }
+}
